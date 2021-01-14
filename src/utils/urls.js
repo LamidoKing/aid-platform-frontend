955cf150e759c04bb4b3bd0b34ddd66266f3ac90
lamido tijjani <kinglamido@gmail.com>
2021-01-14 05:34:05 +0100

    ch:makesome changes

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,8 +1,8 @@
-const api = "http://localhost:3000/api/v1"
+// const api = "http://localhost:3000/api/v1"
+
+const api = "https://aid-platform-api.herokuapp.com/api/v1"
 
 const cableUrl = "ws://localhost:3000/cable"
-
-// const api = "https://aid-platform-api.herokuapp.com/api/v1"
 
 // const cableUrl = "wss://aid-platform-api.herokuapp.com/cable"
 
